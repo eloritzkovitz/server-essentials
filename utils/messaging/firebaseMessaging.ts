483f4f9aaa6f6a55599f871f55aaf16a35ba6535
eloritzkovitz <elor2200@gmail.com>
2025-08-24 13:39:49 +0300

    Small fix

--- utils/messaging/firebaseMessaging.ts
+++ utils/messaging/firebaseMessaging.ts
@@ -6,23 +6,6 @@ import 'dotenv/config';
 
 const SCOPES = ["https://www.googleapis.com/auth/firebase.messaging"];
 
-// Load service account from GOOGLE_APPLICATION_CREDENTIALS env variable
-const credentialsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-if (!credentialsPath) {
-  throw new Error("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.");
-}
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.resolve(credentialsPath), "utf8")
-);
-
-const PROJECT_ID = serviceAccount.project_id;
-
-const jwtClient = new JWT({
-  email: serviceAccount.client_email,
-  key: serviceAccount.private_key,
-  scopes: SCOPES,
-});
-
 export async function sendFcmHttpV1({
   token,
   notification,
@@ -36,6 +19,21 @@ export async function sendFcmHttpV1({
   android?: any;
   webpush?: any;
 }) {
+  const credentialsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+  if (!credentialsPath) {
+    throw new Error("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.");
+  }
+  const serviceAccount = JSON.parse(
+    fs.readFileSync(path.resolve(credentialsPath), "utf8")
+  );
+  const PROJECT_ID = serviceAccount.project_id;
+
+  const jwtClient = new JWT({
+    email: serviceAccount.client_email,
+    key: serviceAccount.private_key,
+    scopes: SCOPES,
+  });
+
   await jwtClient.authorize();
   const accessToken = jwtClient.credentials.access_token;
 
